2a77ac155c81e4ff1be8e37be3dc5ab4e36812d2
tolak <kalot.wang@gmail.com>
2024-04-18 21:38:33 +0800

    script injection

--- engine/lib.rs
+++ engine/lib.rs
@@ -4,13 +4,31 @@
 
 extern crate alloc;
 
+pub use jtee_engine::*;
+
 #[ink::contract]
 mod jtee_engine {
-    use alloc::string::String;
-    use alloc::vec;
-    use alloc::vec::Vec;
+    use alloc::{format, string::String, vec, vec::Vec};
     use phat_js as js;
+    use pink_extension::chain_extension::{signing, SigType};
     use scale::{Decode, Encode};
+
+    pub trait ToArray<T, const N: usize> {
+        fn to_array(&self) -> [T; N];
+    }
+
+    impl<T, const N: usize> ToArray<T, N> for Vec<T>
+    where
+        T: Default + Copy,
+    {
+        fn to_array(&self) -> [T; N] {
+            let mut arr = [T::default(); N];
+            for (a, v) in arr.iter_mut().zip(self.iter()) {
+                *a = *v;
+            }
+            arr
+        }
+    }
 
     #[derive(Debug, Encode, Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
@@ -46,12 +64,26 @@ mod jtee_engine {
     }
 
     #[ink(storage)]
-    pub struct JteeEngine {}
+    pub struct JteeEngine {
+        pub key: [u8; 32],
+        pub account: [u8; 20],
+    }
 
     impl JteeEngine {
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self {}
+            let key = pink_web3::keys::pink::KeyPair::derive_keypair(b"engine").private_key();
+            let ecdsa_pubkey: [u8; 33] = signing::get_public_key(&key, SigType::Ecdsa)
+                .try_into()
+                .expect("Public key should be of length 33");
+            let mut ecdsa_address = [0u8; 20];
+            ink_env::ecdsa_to_eth_address(&ecdsa_pubkey, &mut ecdsa_address)
+                .expect("Get address of ecdsa failed");
+
+            Self {
+                key,
+                account: ecdsa_address,
+            }
         }
 
         #[ink(message)]
@@ -75,7 +107,10 @@ mod jtee_engine {
                 if engine.with_polyfill() {
                     Ok(js::eval_async_js(&js_code, &args))
                 } else {
-                    Ok(pink::ext().js_eval(vec![js::JsCode::Source(js_code)], args))
+                    Ok(pink::ext().js_eval(
+                        vec![js::JsCode::Source(self.seal()), js::JsCode::Source(js_code)],
+                        args,
+                    ))
                 }
             } else {
                 js::eval_with(engine.into_delegate_code_hash()?, &js_code, &args).map(Into::into)
@@ -100,6 +135,17 @@ mod jtee_engine {
             let js_code = String::from_utf8(response.body).map_err(|_| "Invalid code")?;
             self.run_js(engine, js_code, args)
         }
+
+        pub fn seal(&self) -> String {
+            String::from(format!(
+                r#"const jtee = {{
+                    "key": "0x{}",
+                    "account": "0x{}",
+                }};"#,
+                hex::encode(self.key),
+                hex::encode(self.account),
+            ))
+        }
     }
 
     pub fn get_driver(name: &str) -> Result<Hash, String> {
@@ -107,5 +153,19 @@ mod jtee_engine {
         let system = pink::system::SystemRef::instance();
         let delegate = system.get_driver(name.into()).ok_or("No JS driver found")?;
         Ok(delegate.convert_to())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::JteeEngine;
+
+    #[test]
+    fn test_key_derivation() {
+        pink_extension_runtime::mock_ext::mock_all_ext();
+
+        let jtee = JteeEngine::default();
+        println!("jtee key: {}", hex::encode(jtee.key));
+        println!("seal: {:?}", jtee.seal())
     }
 }
