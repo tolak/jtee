55bed739baa09bfff116ef7899d9082f5f73c4d7
tolak <kalot.wang@gmail.com>
2024-04-22 23:18:32 +0800

    wip: deploy engine

---
+++ src/deploy.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const { signAndSend, signCertificate, OnChainRegistry, options, PinkCodePromise } = require('@phala/sdk');
+const { ApiPromise, Keyring, WsProvider } = require('@polkadot/api');
+const BN = require('bn.js');
+
+/**
+ * Deploy smart contract to Phala network.
+ */
+async function deployContract(uri, endpoint) {
+    const targetFile = 'src/engine/jtee_engine.contract'
+    if (!fs.existsSync(targetFile)) {
+        return new Error(`${targetFile} not exists.`)
+    }
+
+    const contractFile = JSON.parse(fs.readFileSync(targetFile, 'utf-8'));
+    console.log(`parsed contract file`);
+
+    // Initialization
+    console.log('Connecting to', endpoint, '...')
+    const provider = new WsProvider(endpoint)
+    const api = await ApiPromise.create(options({ provider, noInitWarn: true }))
+    console.log('Connected.')
+
+    const keyring = new Keyring({ type: 'sr25519' })
+    const user = keyring.addFromUri(uri)
+    const accountInfo = await api.query.system.account(user.address)
+    const free = accountInfo.data.free.div(new BN(1e12)).toNumber()
+    if (free < 20) {
+      console.error('Not enough balance. Please transfer some tokens not less then 20 PHA to', user.address)
+      return new Error('Insufficient Balance')
+    }
+    console.log(`Account ${user.address} has ${free} PHA.`)
+
+    const phatRegistry = await OnChainRegistry.create(api)
+
+    const clusterId = phatRegistry.clusterId
+    // const clusterInfo = phatRegistry.clusterInfo
+    const pruntimeURL = phatRegistry.pruntimeURL
+    console.log('Cluster ID:', clusterId)
+    console.log('Pruntime Endpoint URL:', pruntimeURL)
+
+    const balance = await phatRegistry.getClusterBalance(user.address)
+    console.log('Cluster Balance:', balance.total.div(new BN(1e12)).toNumber())
+  
+    if (balance.free.div(new BN(1e12)).toNumber() < 500) {
+        console.log('Transfer to cluster...')
+        try {
+            await signAndSend(phatRegistry.transferToCluster(user.address, 1e12 * 500), user)
+        } catch (err) {
+            console.log(`Transfer to cluster failed: ${err}`)
+            console.error(err)
+            return new Error('Transfer Error')
+        }
+    }
+
+    const cert = await signCertificate({ pair: user })
+
+    // All prepare conditions ready. Upload with PinkCodePromise
+    console.log('Upload codes...')
+
+    const codePromise = new PinkCodePromise(api, phatRegistry, contractFile, contractFile.source.wasm)
+    const uploadResult = await signAndSend(codePromise.tx.new(), user)
+    // await uploadResult.waitFinalized(user, cert)
+    await uploadResult.waitFinalized()
+    console.log('Code ready in cluster.')
+
+    // Instantiate with PinkBlueprintPromise
+    console.log('Instantiating...')
+    const instantiateResult = await uploadResult.blueprint.send.new({ pair: user, cert, address: cert.address })
+    await instantiateResult.waitFinalized()
+
+    const { contractId } = instantiateResult
+    console.log('Contract uploaded & instantiated: ', contractId)
+}
+
+module.exports = {
+    deployContract,
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const { program } = require('commander');
+require('dotenv').config();
 
+const { program } = require('commander');
 const { createProject } = require('./new');
+const { deployContract } = require('./deploy');
 
 program
     .option('--chain <chain>', 'Chain name that the script deploy on', 'phala')
@@ -11,6 +13,21 @@ const newProject = program
 .argument('<string>', 'Name of the new project')
 .action(projectName => {
     createProject(projectName);
+});
+
+const deployEngine = program
+.command('deploy')
+.description('Deploy engine contract to Phala network.')
+.action(async () => {
+    let uri = process.env.PHALA_ACCOUNT_URI;
+    if (uri === null) {
+        return new Error('Missing Account URI');
+    }
+    let endpoint = process.env.PHALA_RPC;
+    if (endpoint === null) {
+        return new Error('Missing RPC endpoint');
+    }
+    await deployContract(uri, process.env.PHALA_RPC);
 });
 
 const runScript = program
