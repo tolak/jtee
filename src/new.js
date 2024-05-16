1a5d6aaa94efe9cbea8cea8562557578da6b4a2e
tolak <kalot.wang@gmail.com>
2024-05-16 22:52:49 +0800

    print error of file copy

--- src/new.js
+++ src/new.js
@@ -10,20 +10,27 @@ function createProject(projectName) {
 
     if (!fs.existsSync(templatePath)) {
         console.error(`${templatePath} couldn't be found on filesystem}`);
-        throw new Error(`${templatePath} couldn't be found on filesystem}`)
+        return;
     }
 
     if (fs.existsSync(projectPath)) {
         console.error(`A project with the name ${projectName} already exists.`);
         return;
     }
+
     fs.ensureDirSync(projectPath);
-    fs.copySync(templatePath, projectPath, {
-        filter: (src, dest) => {
-            const excludes = ['node_modules', 'dist', '*.lock', '.git', '.DS_Store'];
-            return !excludes.some(dir => src.includes(dir));
-        }
-    });
+    console.log(`Trying copy files from ${templatePath} to ${projectName}}`);
+    try {
+        fs.copySync(templatePath, projectPath, {
+            filter: (src, dest) => {
+                const excludes = ['node_modules', 'dist', '*.lock', '.git', '.DS_Store'];
+                return !excludes.some(dir => src.includes(dir));
+            }
+        });
+    } catch (err) {
+        console.error(`An error occurred while copying files from ${templatePath} to ${projectPath}`);
+        console.error(err);
+    }
 
     // Set the "name" in package.json
     const packageJsonPath = path.join(projectPath, 'package.json');
